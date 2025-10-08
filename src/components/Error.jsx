9bb461b290940e30b54b466abfd6db394dd019a3
saurabhshelake07 <saurabhshelake5051@gmail.com>
2025-10-08 11:45:06 +0530

    Error Page Changes

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -3,8 +3,8 @@ import React from 'react'
 function Error() {
   return (
     <div className='container text-center text-danger mt-4 p-3'>
-        <h1>404</h1>
-        <h>Page Not Found</h>
+      <h1 style={{ fontSize: '5rem' }}>404</h1>
+      <h2 style={{ fontSize: '2.5rem' }}>Page Not Found</h2>
     </div>
   )
 }
